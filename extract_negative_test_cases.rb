f5b688ff4e9aa681e79dc25255c42d583a8dcdc2
Stewie (Nicholas Stewart) <nstewart@instructure.com>
2016-06-15 09:08:27 -0600

    same number of postive and negative test cases

--- extract_negative_test_cases.rb
+++ extract_negative_test_cases.rb
@@ -2,8 +2,13 @@ def select_negative_test_cases(lines)
   lines.select {|line| line.index("false,") }
 end
 
+def select_positive_test_cases(lines)
+  lines.select {|line| line.index("true,") }
+end
+
 def remove_junk(lines)
   lines
+    .map {|line| line.gsub("true,", '') }
     .map {|line| line.gsub("false,", '') }
     .map {|line| line.gsub(/\s+|\"|:/, '') }
 end
@@ -12,6 +17,16 @@ File.open('negative_test_cases.txt', 'w') do |file_handle|
   file_handle.write(
     remove_junk(
       select_negative_test_cases(
+        IO.readlines('test_cases.txt')
+      )
+    ).join("\n")
+  )
+end
+
+File.open('positive_test_cases.txt', 'w') do |file_handle|
+  file_handle.write(
+    remove_junk(
+      select_positive_test_cases(
         IO.readlines('test_cases.txt')
       )
     ).join("\n")
--- readme.md
+++ readme.md
@@ -9,4 +9,7 @@ Build a set of positive and negative word matches
     for i in {1..10000}; do curl -L https://hola.org/challenges/word_classifier/testcase >> test_cases.txt; done
     ruby extract_negative_test_cases.rb
 
+Make sure there are the same number of positive and negative test cases
 
+    head -n 550000 negative_test_cases.txt > tmp_file && mv tmp_file negative_test_cases.txt
+    head -n 550000 positive_test_cases.txt > tmp_file && mv tmp_file positive_test_cases.txt
