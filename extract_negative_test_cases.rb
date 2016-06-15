3b5c794764487045d28538d1098bd5a1264d60ff
Stewie (Nicholas Stewart) <nstewart@instructure.com>
2016-06-15 09:14:24 -0600

    Downcase all the things

--- extract_negative_test_cases.rb
+++ extract_negative_test_cases.rb
@@ -13,11 +13,17 @@ def remove_junk(lines)
     .map {|line| line.gsub(/\s+|\"|:/, '') }
 end
 
+def downcase(lines)
+  lines.map {|line| line.downcase }
+end
+
 File.open('negative_test_cases.txt', 'w') do |file_handle|
   file_handle.write(
-    remove_junk(
-      select_negative_test_cases(
-        IO.readlines('test_cases.txt')
+    downcase(
+      remove_junk(
+        select_negative_test_cases(
+          IO.readlines('test_cases.txt')
+        )
       )
     ).join("\n")
   )
@@ -25,9 +31,11 @@ end
 
 File.open('positive_test_cases.txt', 'w') do |file_handle|
   file_handle.write(
-    remove_junk(
-      select_positive_test_cases(
-        IO.readlines('test_cases.txt')
+    downcase(
+      remove_junk(
+        select_positive_test_cases(
+          IO.readlines('test_cases.txt')
+        )
       )
     ).join("\n")
   )
